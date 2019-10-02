b6bc08f3f7856ed540956b85464bd362af14de63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-02 12:28:40 +0000

    Deploy alexmorphine/2019-2-Atom-Frontend-A-Ovsyannikova to github.com/alexmorphine/2019-2-Atom-Frontend-A-Ovsyannikova.git:gh-pages

---
+++ precache-manifest.85af3ab44582d0ecc21496ee1982835e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1aaed789cf37aaaf25c83b318e8d0a",
+    "url": "/atom-project/index.html"
+  },
+  {
+    "revision": "677df32de2cfa98b4a26",
+    "url": "/atom-project/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "1f1a36090f82ebf670ee",
+    "url": "/atom-project/static/js/2.8d7d6c42.chunk.js"
+  },
+  {
+    "revision": "677df32de2cfa98b4a26",
+    "url": "/atom-project/static/js/main.8309640b.chunk.js"
+  },
+  {
+    "revision": "1c4a7b006f2f800c32fd",
+    "url": "/atom-project/static/js/runtime-main.ba9a1f37.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/atom-project/static/media/logo.a55f1c43.svg"
+  }
+]);
